dcd3df47ed4c71dcbab2566d65b7b27bae006725
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-02-08 08:47:33 -0300

    fix: remove alias for database

--- src/main/src/repositories/drizzle/drizzle-products-repository.ts
+++ src/main/src/repositories/drizzle/drizzle-products-repository.ts
@@ -1,6 +1,6 @@
 import { ProductType } from '@schemas/@exports'
 import { ProductsRepository } from '../products-repository'
-import { db } from '@database/index'
+import { db } from '../../../../database'
 
 export class DrizzleProductsRepository implements ProductsRepository {
   async findMany(): Promise<ProductType[]> {

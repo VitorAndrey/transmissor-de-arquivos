864ee2dfb04a721a20fa777a933f329e29e8c7e5
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-03-19 10:10:33 -0300

    chore: remove redundant app.tsx

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,22 @@ import './styles/main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app'
+
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
+import { routeTree } from './route-tree.gen'
+
+const hashHistory = createHashHistory()
+
+const router = createRouter({ routeTree, history: hashHistory })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
